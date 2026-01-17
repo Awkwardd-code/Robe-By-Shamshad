22a5ed1203d6096ea2a37a3ef30f6fda682e6f29
Awkwardd-code <somethinn999awkwardd@gmail.com>
2026-01-17 17:07:52 +0600

    meta pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import ToastProvider from "@/components/ToastProvider";
 import { AuthProvider } from "@/context/AuthContext";
 import { CommerceProvider } from "@/context/CommerceContext";
@@ -56,7 +57,7 @@ export const metadata: Metadata = {
     description:
       "Discover Robe by Shamshad, your go-to destination for trendy and high-quality clothing and apparel. Explore our diverse collection and elevate your style today.",
     images: ["/logo.jpg"],
-  },  
+  },
 };
 
 export default async function RootLayout({
@@ -68,7 +69,34 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+        <Script
+          id="meta-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `!function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '857404480130156');
+                fbq('track', 'PageView');`,
+          }}
+        />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=857404480130156&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         <AuthProvider initialUser={initialUser}>
           <CommerceProvider>
             <BuyNowProvider>
