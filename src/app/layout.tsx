ed30c43df61b703168c1887a5b9c1fed164da332
Awkwardd-code <somethinn999awkwardd@gmail.com>
2026-02-09 16:09:47 +0600

    google tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,8 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+const gtmId = process.env.NEXT_PUBLIC_GTM_ID;
+
 export const viewport = { width: "device-width", initialScale: 1 };
 
 export const themeColor = [
@@ -71,6 +73,19 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <head>
+        {gtmId ? (
+          <Script
+            id="gtm"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${gtmId}');`,
+            }}
+          />
+        ) : null}
         <Script
           id="meta-pixel"
           strategy="afterInteractive"
@@ -105,6 +120,16 @@ export default async function RootLayout({
         />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        {gtmId ? (
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            />
+          </noscript>
+        ) : null}
         <MetaCapi />
         <noscript>
           <img
